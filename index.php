f40adb28054e093854ddbf223e4afa95dba9c5d4
grimkroton <2418344+grimkroton@users.noreply.github.com>
2024-10-16 13:33:05 +0000

    trophy layout

--- index.php
+++ index.php
@@ -1,31 +1,18 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Azure Webanwendung</title>
-</head>
+<head><script initial="">if (!('ResizeObserver' in window)) {    window.location.search='v-r=oldbrowser';}</script><script initial="">window.Vaadin = window.Vaadin || {};window.Vaadin.TypeScript= {};</script><meta name="_csrf_parameter" content="_csrf"><meta name="_csrf_header" content="X-CSRF-TOKEN"><meta name="_csrf" content="f0a27a0e-4121-4dba-a75b-79a07d848ab6"><base href="."><script type="text/javascript">window.JSCompiler_renameProperty = function(a) { return a;}</script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body, #outlet {
+      height: 100vh;
+      width: 100%;
+      margin: 0;
+    }
+  </style>  
+  <script type="module" crossorigin src="main.js"></script>
+<style>.v-reconnect-dialog,.v-system-error {position: absolute;color: black;background: white;top: 1em;right: 1em;border: 1px solid black;padding: 1em;z-index: 10000;max-width: calc(100vw - 4em);max-height: calc(100vh - 4em);overflow: auto;} .v-system-error {color: indianred;pointer-events: auto;} .v-system-error h3, .v-system-error b {color: red;}</style><style>[hidden] { display: none !important; }</style></head>
 <body>
-	<header>
-		<nav>
-			<ul>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<section>
-			<h1>Willkommen auf der ersten Azure Webanwendung!</h1>
-      <h2>Diese Webanwendung wurde aus dem Code von diesem <a href="https://github.com/grimkroton/microapp">Repo</a> automatisiert ausgerollt</h2>
-      <?php
-        echo getHostByName(getHostName());
-	echo phpinfo();
-      ?>
-		</section>
-	</main>
-	<footer>
-		<p></p>
-	</footer>
+  <!-- This outlet div is where the views are rendered -->
+  <div id="outlet"></div>
 </body>
 </html>
-
